b3936d93bee076dcc872a3e7a36cac725fc81291
Ekaterina <katyshik2000@yandex.com>
2023-01-08 18:43:42 +0300

    commit

--- advent.py
+++ advent.py
@@ -2,6 +2,7 @@ import logging
 import telebot
 from os import getenv
 from time import sleep
+from datetime import date, datetime
 from telebot import types
 from sqlalchemy import create_engine
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -38,6 +39,7 @@ def start2_message(message):
     bot.send_message(message.chat.id, 'ПРАВИЛА АДВЕНТ КАЛЕНДАРЯ')
     bot.send_message(message.chat.id, '1. Каждый день тебе будет приходить ссылка на небольшое видео с теорией + конспект. За просмотр видео ты получаешь автоматически 5 баллов.\n\n2. После видео тебе необходимо прорешать 3 задания с выбором ответа, за каждое из которых ты можешь заработать 2 балла.\n\n3.Если ты хочешь больше практики, а также получить ещё дополнительные баллы, то тебе будет доступна ссылка на гугл-форму. Там будут дополнительные задания, некоторые с ручной проверкой (например, cочинения). За решение гугл-форм тебе начислятся баллы 30 декбря.\n\n4. Если ты хочешь получить подробный разбор сочинения с рекомендациями, как исправить ошибки и улучшить результат, то внимательно смотри задания в гугл-формах)', reply_markup=keyboard)
 
+
 @bot.message_handler(content_types=['text', ])
 def register_message(message):
     msg = bot.reply_to(message, 'Отлично, теперь ты знаешь, что тебя ждёт!\n\nСкажи, пожалуйста, как тебя зовут. Напиши свои имя и фамилю одним сообщением.\n\nНапример:\nИван Иванов')
@@ -46,21 +48,15 @@ def register_message(message):
 @bot.message_handler(content_types=['text', ])
 def get_name(message):
     keyboard = types.InlineKeyboardMarkup()
-    cont = types.InlineKeyboardButton(
-        text='Жду задания!', callback_data='yes3')
+    cont = types.InlineKeyboardButton(text='Жду задания!', callback_data='yes3')
     keyboard.add(cont)
     current_user = message.chat.username
     res = engine.execute(f"SELECT COUNT(*) FROM members WHERE username = '{current_user}'").fetchall()[0][0]
     if res == 0:
         engine.execute(
             f'''INSERT INTO members (username, user_id, name, score) VALUES ('{current_user}', '{message.chat.id}', '{message.text}', '{0}');''')
-    bot.reply_to(
-        message, 'Приятно познакомиться!\nЖалаю удачи!\n\nИ помни, если у тебя возникнут какие-либо воросы, ты всегда можешь обратиться ко мне в личные сообщения.\n\nМой тг: @wirsme')
-
-@bot.message_handler(content_types=['text', ])
-def prompt(message):
-    bot.send_message(message.chat.id, 'gyuk')
-    scheduler.add_job(prompt(message), 'date', run_date='2023-1-07 19:23:00')
+    bot.send_message(
+        message.chat.id, 'Приятно познакомиться!\nЖалаю удачи!\n\nИ помни, если у тебя возникнут какие-либо воросы, ты всегда можешь обратиться ко мне в личные сообщения.\n\nМой тг: @wirsme', reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -68,13 +64,17 @@ def callback_worker(call):
         start2_message(call.message)
     if call.data == "yes2":
         register_message(call.message)
+    if call.data == "yes3":
+        scheduler.add_job(prompt(call.message), 'date', run_date=datetime(2023, 1, 8, hour=15, minute=8))
 
+@bot.message_handler(content_types=['text', ])
+def prompt(message):
+    bot.send_message(message.chat.id, 'gyuk')
 
 if __name__ == '__main__':
     try:
-        bot.polling(none_stop=True)
         scheduler.start()
-        print(scheduler.get_jobs())
+        bot.polling(none_stop=True)
         while True:
             sleep(1)
     except:
